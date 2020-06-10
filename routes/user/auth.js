ed2f1983dfa666d6045f7da60f12917348e7856e
Ahmet Halilovic <akihalilovic@gmail.com>
2020-06-11 00:52:59 +0200

    Update auth.js

--- routes/user/auth.js
+++ routes/user/auth.js
@@ -130,7 +130,9 @@ router.post('/login', async (req, res) => {
     return res.status(400).send('Molim Vas potvrdite svoj mejl!');
 
   // create and assign a token
-  const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+  const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET, {
+    expiresIn: '1h',
+  });
   res.header('auth-token', token).send(token);
 });
 
