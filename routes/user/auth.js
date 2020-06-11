451fbf8bea8a51f6187dc0fa95533c933fbdd2f1
Ahmet Halilovic <akihalilovic@gmail.com>
2020-06-11 02:16:06 +0200

    Update auth.js

--- routes/user/auth.js
+++ routes/user/auth.js
@@ -131,7 +131,7 @@ router.post('/login', async (req, res) => {
 
   // create and assign a token
   const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET, {
-    expiresIn: 1,
+    expiresIn: 5000,
   });
   res.header('auth-token', token).send(token);
 });
