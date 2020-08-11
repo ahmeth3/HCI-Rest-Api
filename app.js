067f9811a416529d78a94bad41a4c8a2e0a90c05
Ahmet Halilovic <akihalilovic@gmail.com>
2020-08-11 18:20:39 +0200

    asdad

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ const consultationRoute = require('./routes/consultation/consultation');
 // Middlewares
 dotenv.config();
 app.use(express.json());
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
 // Route Middlewares
 app.use('/user', authRoute);
